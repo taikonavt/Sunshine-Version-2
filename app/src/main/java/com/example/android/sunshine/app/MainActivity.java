5a450442b6aac393c92a89e78ccb2228fd17b685
taikonavt <taikonavt@gmail.com>
2016-06-22 18:28:43 +0300

    set adapter to ListView

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -56,6 +57,7 @@ public class MainActivity extends ActionBarActivity {
     public static class PlaceholderFragment extends Fragment {
 
         ArrayAdapter<String> mForecastAdapter;
+        ListView listView;
 
         public PlaceholderFragment() {
         }
@@ -63,6 +65,8 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
             String [] forecastArray = {
                     "Today - Sunny - 88/63",
@@ -79,7 +83,10 @@ public class MainActivity extends ActionBarActivity {
             mForecastAdapter = new ArrayAdapter<String> (getActivity(),
                     R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
 
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+
+            listView.setAdapter(mForecastAdapter);
+
             return rootView;
         }
     }
